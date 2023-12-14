a137f7e70a800b5fd50dea61541ff49ef2aae8ea
RikLubbers <119963960+RikLubbers@users.noreply.github.com>
2023-12-14 12:09:13 +0100

    Refactor displace function to exclude open water and flooding areas

--- Code/Simulate_DHS_location.py
+++ Code/Simulate_DHS_location.py
@@ -3,38 +3,27 @@ import rasterio
 from shapely.geometry import box
 from shapely.geometry import Point 
 from shapely.ops import cascaded_union 
-from scipy.spatial import distance 
 
 """_summary_
-# This function generates random points within a buffer around DHS clusters, excluding open water and flooding areas, and within the admin boundary.
-# Inputs:
-# pts: list of tuples of coordinates of DHS clusters
-# admin: shapely polygon of admin boundary
-# samp_num: number of random points to sample
-# other_num: number of random points to generate
-# landcover_raster: path to landcover raster
-# flood_raster: path to flood raster
+This function generates random points within a buffer around DHS clusters, excluding open water and flooding areas, and within the admin boundary.
 
-# Outputs:
-# r_pts: list of arrays of coordinates of random points
+Inputs:
+- pts: list of tuples of coordinates of DHS clusters
+- admin: shapely polygon of admin boundary
+- samp_num: number of random points to sample
+- other_num: number of random points to generate
+- open_water: shapely polygon of open water areas
+- flood_area: shapely polygon of flooding areas
 
-# Example:
-# pts = [(0, 0), (1, 1)]
-# admin = Polygon([(0, 0), (0, 5), (5, 5), (5, 0)])
-# samp_num = 2
-# other_num = 5
-# landcover_raster = 'path_to_landcover_raster'
-# flood_raster = 'path_to_flood_raster'
-# r_pts = displace(pts, admin, samp_num, other_num, landcover_raster, flood_raster)
-# print(r_pts)
+Outputs:
+- r_pts: list of arrays of coordinates of random points
 
-# Author: Rik Lubbers
-# Date: 2023-12-05
-# Version: 1.0
-# License: MIT License
+Author: Rik Lubbers
+Date: 2023-12-05
+Version: 1.0
 """
 
-def displace(pts, admin, samp_num, other_num, landcover_raster, flood_raster):
+def displace(pts, admin, samp_num, other_num, open_water, flood_area):
     n = len(pts)
     offset_dist = 2000 #2 km buffer as it is an urban DHS cluster
 
@@ -46,22 +35,13 @@ def displace(pts, admin, samp_num, other_num, landcover_raster, flood_raster):
         pt = Point(pts[i]) # transform to shapely point
         buffer = pt.buffer(offset_dist) # buffer around point
 
-        # Create a polygon of the open water areas
-        with rasterio.open(landcover_raster) as src:
-            open_water = box(*src.bounds)
-            for value, (j, i) in src.sample([(pt.x, pt.y)], indexes=1):
-                if value == 10:
-                    open_water = open_water.union(box(i, j, i + 1, j + 1))
+        # Check if buffer is a GeometryCollection
+        if buffer.geom_type == 'GeometryCollection':
+            # If buffer is a GeometryCollection, take the union of all geometries
+            buffer = cascaded_union(buffer)
 
         # Subtract the open water areas from the buffer
         buffer = buffer.difference(open_water)
-
-        # Create a polygon of the flooding areas
-        with rasterio.open(flood_raster) as src:
-            flood_area = box(*src.bounds)
-            for value, (j, i) in src.sample([(pt.x, pt.y)], indexes=1):
-                if value == 1:
-                    flood_area = flood_area.union(box(i, j, i + 1, j + 1))
 
         # Subtract the flooding areas from the buffer
         buffer = buffer.difference(flood_area)
@@ -87,36 +67,81 @@ def displace(pts, admin, samp_num, other_num, landcover_raster, flood_raster):
 
     return r_pts
 
+# Apply function
+pts = [(3, 4), (5, 6), (7, 8)]
+admin = box(0, 0, 10, 10)
+samp_num = 10
+other_num = 1000
+open_water = box(0, 0, 2, 2)
+flood_area = box(0, 0, 1, 1)
 
-## Test
-import unittest
-from shapely.geometry import Polygon
-import numpy as np
+displace(pts, admin, samp_num, other_num, open_water, flood_area)
+from scipy.spatial import distance 
 
-class TestDisplace(unittest.TestCase):
-    def setUp(self):
-        self.pts = [(0, 0), (1, 1)]
-        self.admin = Polygon([(0, 0), (0, 5), (5, 5), (5, 0)])
-        self.samp_num = 2
-        self.other_num = 5
-        self.landcover_raster = 'path_to_landcover_raster'
-        self.flood_raster = 'path_to_flood_raster'
+"""_summary_
+This function generates random points within a buffer around DHS clusters, excluding open water and flooding areas, and within the admin boundary.
 
-    def test_displace_returns_correct_shape(self):
-        result = displace(self.pts, self.admin, self.samp_num, self.other_num, self.landcover_raster, self.flood_raster)
-        self.assertEqual(len(result), len(self.pts))
-        for r_pts_i in result:
-            self.assertEqual(r_pts_i.shape, (self.samp_num, 2))
+Inputs:
+- pts: list of tuples of coordinates of DHS clusters
+- admin: shapely polygon of admin boundary
+- samp_num: number of random points to sample
+- other_num: number of random points to generate
+- open_water: shapely polygon of open water areas
+- flood_area: shapely polygon of flooding areas
 
-    def test_displace_returns_points_within_admin(self):
-        result = displace(self.pts, self.admin, self.samp_num, self.other_num, self.landcover_raster, self.flood_raster)
-        for r_pts_i in result:
-            for pt in r_pts_i:
-                self.assertTrue(self.admin.contains(Point(pt)))
+Outputs:
+- r_pts: list of arrays of coordinates of random points
 
-    def test_displace_returns_different_points_for_different_input_points(self):
-        result = displace(self.pts, self.admin, self.samp_num, self.other_num, self.landcover_raster, self.flood_raster)
-        self.assertFalse(np.array_equal(result[0], result[1]))
+Author: Rik Lubbers
+Date: 2023-12-05
+Version: 1.0
+"""
 
-if __name__ == '__main__':
-    unittest.main()
+def displace(pts, admin, samp_num, other_num, open_water, flood_area):
+    n = len(pts)
+    offset_dist = 2000 #2 km buffer as it is an urban DHS cluster
+
+    r_pts = []
+    for i in range(n):
+        r_pts_i = np.zeros((samp_num, 2)) # Matrix to store random points
+
+        # Buffer around point
+        pt = Point(pts[i]) # transform to shapely point
+        buffer = pt.buffer(offset_dist) # buffer around point
+
+        # Subtract the open water areas from the buffer
+        buffer = buffer.difference(open_water)
+
+        # Subtract the flooding areas from the buffer
+        buffer = buffer.difference(flood_area)
+
+        # Intersection with admin
+        intersection = buffer.intersection(admin) # intersection with admin boundary to ensure points are within the admin boundary
+
+        # Generating random points
+        if intersection.is_empty:
+            # If no intersection, generate random points within the buffer
+            random_pts = [Point(buffer.exterior.coords[i]) for i in range(other_num)]
+        else:
+            # If intersection exists, generate random points within the intersection
+            random_pts = [Point(intersection.exterior.coords[i]) for i in range(other_num)]
+
+        # Sample random points
+        probs = 1 / distance.cdist([pts[i]], [pt.coords[0] for pt in random_pts])
+        probs /= np.sum(probs)
+        indices = np.random.choice(range(len(random_pts)), size=samp_num, p=probs)
+        r_pts_i = np.array([random_pts[idx].coords[0] for idx in indices])
+
+        r_pts.append(r_pts_i)
+
+    return r_pts
+
+# Apply function
+pts = [(3, 4), (5, 6), (7, 8)]
+admin = box(0, 0, 10, 10)
+samp_num = 10
+other_num = 1000
+open_water = box(0, 0, 2, 2)
+flood_area = box(0, 0, 1, 1)
+
+displace(pts, admin, samp_num, other_num, open_water, flood_area)
